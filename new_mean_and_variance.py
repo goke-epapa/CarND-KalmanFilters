631ee6739eae0092029dc9f6a3fb481862d9f170
Adegoke Obasa <adegokeobasa@gmail.com>
2019-07-30 08:04:29 +0200

    add quiz to predict new values

--- new_mean_and_variance.py
+++ new_mean_and_variance.py
@@ -10,3 +10,10 @@ def update(mean1, var1, mean2, var2):
     return [new_mean, new_var]
 
 print(update(10.,8.,13., 2.))
+
+def predict(mean1, var1, mean2, var2):
+    new_mean = mean1 + mean2
+    new_var = var1 + var2
+    return [new_mean, new_var]
+
+print(predict(10., 4., 12., 4.))
